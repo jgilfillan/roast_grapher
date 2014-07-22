cb58b54df0cb50beed1db8684c84252dabe2266a
Josh Gilfillan <j.gilfillan+dev@gmail.com>
2014-07-22 18:44:06 +1000

    fixed nav button spacing

--- RoastProfiler.js
+++ RoastProfiler.js
@@ -1,13 +1,13 @@
 //dropbox button
 var dropBoxOptions = {
-	success: readDropBoxFile,
-	linkType: 'direct',
-	multiselect: false,
-	extensions: ['.csv']
-	};
+  success: readDropBoxFile,
+  linkType: 'direct',
+  multiselect: false,
+  extensions: ['.csv']
+  };
 
 function dropBoxOnClick() {
-	Dropbox.choose(dropBoxOptions);
+  Dropbox.choose(dropBoxOptions);
 }
 
 // var button = Dropbox.createChooseButton(dropBoxOptions);
@@ -17,13 +17,11 @@ function dropBoxOnClick() {
 var csvData;
 
 // responsive variables
-var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    xSize = parseInt(d3.select('.chart-area').style('width')),
+var xSize = parseInt(d3.select('.chart-area').style('width')),
     ySize = xSize / 1.618,
     ySizeTempChange = 0.35 * xSize;
+
+    console.log(xSize);
 
 
 //chart setup
@@ -32,6 +30,8 @@ var margin = {top: 10, right: 10, bottom: 30, left: 35},
     height = ySize - margin.top - margin.bottom,
     heightTemp = (ySize - margin.top - margin.bottom) * 0.65,
     heightTempChange = (ySize - margin.top - margin.bottom) * 0.35;
+
+
 
 var x = d3.time.scale()
     .range([0, width]);
@@ -80,98 +80,98 @@ var yAxisTempg = svgTemp.append("g")
 
  //formatter for seconds to mm:ss
  function secondsFormatter(d) {
- 	var min = Math.floor((+d)/60);
- 	var sec = (+d - (Math.floor((+d)/60) * 60));
- 	return min + ':' + ((sec < 10) ? '0' + sec : sec);
+  var min = Math.floor((+d)/60);
+  var sec = (+d - (Math.floor((+d)/60) * 60));
+  return min + ':' + ((sec < 10) ? '0' + sec : sec);
  }
 
 
 //filter to exclude data after pull
 function excludeDataAfterPull(d) {
-	var maxTemp = d3.max(d.filter(function(d) { return (+d.Time > 60); }), function(d) {return +d.Value1; });	// higest temp afer 60 seconds into roast
+  var maxTemp = d3.max(d.filter(function(d) { return (+d.Time > 60); }), function(d) {return +d.Value1; }); // higest temp afer 60 seconds into roast
 
-	//time at max temp
-	var maxTempTime = d3.max(
-		d.filter(function(d) {return (+d.Time > 60 && +d.Value1 === maxTemp); }),	//rows that match temp after 60 sec into roast
-		function(d) {return +d.Time + 20;}
-		);
+  //time at max temp
+  var maxTempTime = d3.max(
+    d.filter(function(d) {return (+d.Time > 60 && +d.Value1 === maxTemp); }), //rows that match temp after 60 sec into roast
+    function(d) {return +d.Time + 20;}
+    );
 
-	return d.filter(function(d) { return (+d.Time <= maxTempTime);  });
+  return d.filter(function(d) { return (+d.Time <= maxTempTime);  });
 
 }
 
 
 function readLocalFile() {
-	csvData = Papa.parse(document.getElementById('csvFile').files[0], {
-		delimiter: "",
-		header: true,
-		dynamicTyping: false,
-		preview: 0,
-		step: undefined,
-		encoding: "",
-		worker: false,
-		comments: false,
-		complete: drawChart,		//callback function
-		download: false
-		}
-	);
+  csvData = Papa.parse(document.getElementById('csvFile').files[0], {
+    delimiter: "",
+    header: true,
+    dynamicTyping: false,
+    preview: 0,
+    step: undefined,
+    encoding: "",
+    worker: false,
+    comments: false,
+    complete: drawChart,    //callback function
+    download: false
+    }
+  );
 
 }
 
 function readDropBoxFile(files) {
-	console.log(files);
-	csvData = Papa.parse(files[0].link, {
-		delimiter: "",
-		header: true,
-		dynamicTyping: false,
-		preview: 0,
-		step: undefined,
-		encoding: "",
-		worker: false,
-		comments: false,
-		complete: drawChart,		//callback function
-		download: true
-		}
-	);
+  console.log(files);
+  csvData = Papa.parse(files[0].link, {
+    delimiter: "",
+    header: true,
+    dynamicTyping: false,
+    preview: 0,
+    step: undefined,
+    encoding: "",
+    worker: false,
+    comments: false,
+    complete: drawChart,    //callback function
+    download: true
+    }
+  );
 }
 
 function drawChart(results, file) {
-	//console.log(results.data);
+  //console.log(results.data);
 
-	var data = excludeDataAfterPull(results.data);
+  var data = excludeDataAfterPull(results.data);
 
-	//format columns to plot
-	data.forEach(function(d) {
-		d.Time = new Date(2000, 1, 1, 0, Math.floor((+d.Time)/60), +d.Time - (Math.floor((+d.Time)/60) * 60), 0);
-		d.Value1 = +d.Value1;
-	});
+  //format columns to plot
+  data.forEach(function(d) {
+    d.Time = new Date(2000, 1, 1, 0, Math.floor((+d.Time)/60), +d.Time - (Math.floor((+d.Time)/60) * 60), 0);
+    d.Value1 = +d.Value1;
+  });
 
-	console.log(data);
+  console.log(data);
 
-	x.domain(d3.extent(data, function(d) { return d.Time; })).ticks(d3.time.minute.utc, 1);
-  	yTemp.domain(d3.extent(data, function(d) { return d.Value1; })).nice();
-  	// y.domain([0,d3.max(data, function(d) { return d.Value1; })] );
+  x.domain(d3.extent(data, function(d) { return d.Time; })).ticks(d3.time.minute.utc, 1);
+    yTemp.domain(d3.extent(data, function(d) { return d.Value1; })).nice();
+    // y.domain([0,d3.max(data, function(d) { return d.Value1; })] );
 
-	xAxisg.attr("transform", "translate(0," + heightTemp + ")")
+  xAxisg.attr("transform", "translate(0," + heightTemp + ")")
       .call(xAxis);
 
-	yAxisTempg.call(yAxisTemp)
-	.append("text")
-	  .attr("transform", "rotate(-90)")
-	  .attr("y", 6)
-	  .attr("dy", ".71em")
-	  .style("text-anchor", "end")
-	  .text("C");
+  yAxisTempg.call(yAxisTemp)
+  .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("ºC");
 
-	var series = svgTemp.selectAll('.line')
-	  .data([data]);
+  var series = svgTemp.selectAll('.line')
+    .data([data]);
 
-	series.exit().remove;
+  series.exit().remove;
 
-	series.enter()
-	  .append("path")
-	  .attr("class", "line")
-	  .attr("d", line);
+  series.enter()
+    .append("path")
+    .attr("class", "line")
+    .attr("d", line);
 
-	series.attr("d", line);
+  series.attr("d", line);
 }
